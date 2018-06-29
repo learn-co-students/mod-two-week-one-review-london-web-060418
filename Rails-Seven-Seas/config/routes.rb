11e6269d7025ec8900d9ae186e765f3d57120854
Allegra <aukseallegra@gmail.com>
2018-06-29 09:44:40 +0100

    Done.

--- Rails-Seven-Seas/config/routes.rb
+++ Rails-Seven-Seas/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get "/", to: "seas#welcome"   #worked !!!!!!!!!!!!!!!!!
+  #what user sees; where it is actually going
+
+  get "/seas", to: "seas#index"
+
+  get "/seas/:id", to: "seas#show"
+
+  get "/seas/:id/edit", to: "seas#edit"
+
 end
